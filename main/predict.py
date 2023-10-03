851fbdb033c0c8ebd326776c515b3e267e85f0c1
cycnlp <39939639+cycnlp@users.noreply.github.com>
2023-10-03 16:31:37 +0800

    Add files via upload

---
+++ main/predict.py
@@ -0,0 +1,287 @@
+import os
+
+import torch
+from medpy import metric
+from torch.utils.data import DataLoader
+# from torchvision.transforms import transforms
+
+import test01
+from utils.dataset import DeeplabDataset
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+from train_val_test_config import BUSI_path
+with open(os.path.join(BUSI_path, "val.txt"), "r") as f:
+    val_lines = f.readlines()
+
+import random
+
+import numpy as np
+seed = 2021
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+
+val_img_ids=val_lines
+
+val_dataset = DeeplabDataset(val_img_ids, BUSI_path, 'test')
+val_gen = DataLoader(val_dataset, batch_size=1, num_workers=0, pin_memory=True, drop_last=True)
+
+from mynets.hynet import net1
+
+model = net1(2)
+model_name = 'hynet'
+# path=''
+
+# model.load_state_dict(torch.load(path,map_location='cpu')['model'])
+# model.load_state_dict(torch.load(path,map_location='cpu')['model'],strict=False)
+#
+# save_file = {"model": model.state_dict()}
+# torch.save(save_file, path1 + "/model_best.pth")
+
+path='/home/zmk/deeplearning/Dataset_BUSI_upup/result/model_best.pth'
+model.load_state_dict(torch.load(path,map_location='cpu')['model'])
+
+
+model=model.to(device)
+model.eval()
+import torch.nn.functional as F
+i=0
+
+# https://blog.csdn.net/u010095372/article/details/102947315
+
+
+# def accuracy(pred_mask, label):
+#     '''
+#     acc=(TP+TN)/(TP+FN+TN+FP)
+#     '''
+#     pred_mask = pred_mask.astype(np.uint8)
+#     TP, FN, TN, FP = [0, 0, 0, 0]
+#     for i in range(label.shape[0]):
+#         for j in range(label.shape[1]):
+#             if label[i][j] == 1:
+#                 if pred_mask[i][j] == 1:
+#                     TP += 1
+#                 elif pred_mask[i][j] == 0:
+#                     FN += 1
+#             elif label[i][j] == 0:
+#                 if pred_mask[i][j] == 1:
+#                     FP += 1
+#                 elif pred_mask[i][j] == 0:
+#                     TN += 1
+#     acc = (TP + TN) / (TP + FN + TN + FP)
+#     # 精确率
+#     Precision = TP / (TP + FP)
+#     # 召回率
+#     sen = TP / (TP + FN)
+#     #精确率
+#
+#     # f1-score
+#     f1=(2*Precision*sen)/(Precision+sen)
+#     fb=((1+0.3*0.3)*Precision*sen)/(0.3*0.3*Precision+sen)
+#     return acc,Precision, sen,f1,fb,0
+
+def calculate_metric_percase(pred, gt):
+    pred[pred > 0] = 1
+    gt[gt > 0] = 1
+    if pred.sum() > 0 and gt.sum()>0:
+        dice = metric.binary.dc(pred, gt)
+        hd95 = metric.binary.hd95(pred, gt)
+        # metric.binary.
+        jc = metric.binary.jc(pred, gt)
+        # return dice, hd95,jc
+        return dice
+    elif pred.sum() > 0 and gt.sum()==0:
+        # return 1, 0,0
+        return 1
+    else:
+        # return 0, 0,0
+        return 0
+seed = 2021
+random.seed(seed)
+np.random.seed(seed)
+torch.manual_seed(seed)
+torch.cuda.manual_seed_all(seed)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+mymiou=[]
+myiu1=[]
+myiu0=[]
+
+mydes=[]
+myse=[]
+mysp=[]
+myacc=[]
+myf1=[]
+mypa=[]
+
+
+myacc_sklearn=[]
+mypre_sklearn=[]
+myrecall_sklearn=[]
+myf1_sklearn=[]
+myauc_sklearn=[]
+
+metric_list = []
+with torch.no_grad():
+
+
+
+    # for x, (imgs, labels, edge) in enumerate(val_gen):
+    for x, (imgs, labels,name) in enumerate(val_gen):
+
+        print(name[0]+'png')
+        imgs=imgs.to(device)
+        labels=torch.squeeze(labels,dim=1)
+        # outputs, out_edge = model(imgs)#outputs是输出的图像 (b c h w)
+        # outputs,_,_ = model(imgs)#outputs是输出的图像 (b 2 h w)  每类对应一个通道，0是第一个通道，1是第二个通道#
+        outputs= model(imgs)#outputs是输出的图像 (b 2 h w)  每类对应一个通道，0是第一个通道，1是第二个通道/
+        outputs= outputs[0]
+        #因为网络最后没有经过softmax  交叉熵函数自带
+        # outx = F.log_softmax(outputs, dim=1)
+
+        outx = F.softmax(outputs, dim=1) # b c h w
+
+
+        print(x)
+
+
+        # ## 判断每个像素的类别
+        # 取出最大值的通道就是類別 batchize c h w--->b h w
+        pre= torch.argmax(outx,dim=1)#b  h w
+
+        # for i in range(1,4):
+
+        pre = pre.data.cpu().numpy()  # 1  h w
+        labels = labels.data.cpu().numpy()
+
+
+        mu = test01.IOUMetric(2)
+        mu.add_batch(pre, labels)
+
+        PA, CPA, MPA, iu, mean_iu, fwavacc = mu.evaluate()
+        mymiou.append(mean_iu)
+        myiu1.append(iu[1])
+        myiu0.append(iu[0])
+
+
+        for i in range(1,2):
+        # for i in range(0,2):
+            # metric_list.append(calculate_metric_percase(pre.data.cpu().numpy().squeeze() == i, labels.data.cpu().numpy().squeeze() == i))
+            metric_list.append(calculate_metric_percase(pre.squeeze() == i, labels.squeeze() == i))
+
+
+        from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score,roc_auc_score
+
+        labels=labels.flatten().squeeze()
+        pre=pre.flatten().squeeze()
+
+        acc=accuracy_score(labels.flatten(),pre.flatten())
+        # pre=precision_score(labels.flatten(),pre.flatten())
+        # print(np.unique(labels))
+        # print(np.unique(pre))
+        pre_score=precision_score(labels.flatten(),pre.flatten())
+        recall=recall_score(labels.astype('int').flatten(),pre.flatten())
+        f1=f1_score(labels.flatten(),pre.flatten())
+        auc=roc_auc_score(labels.flatten(),pre.flatten())
+
+        myacc_sklearn.append(acc)
+        mypre_sklearn.append(pre_score)
+        myrecall_sklearn.append(recall)
+        myf1_sklearn.append(f1)
+        myauc_sklearn.append(auc)
+
+        # axx,Precision,sen ,f1,FB,dsc= accuracy( pre.squeeze(), labels.squeeze())
+        # mydes.append(dsc)
+        # myrecall.append(sen)
+        # myPrecision.append(Precision)
+
+print(model_name)
+print(path)
+print("miou",np.mean(mymiou), np.std(mymiou,ddof=1))
+print("iou1",np.mean(myiu1),np.std(myiu1,ddof=1))
+print("iou0",np.mean(myiu0),np.std(myiu0,ddof=1))
+#
+print("这是sklearn的结果")
+print("acc",np.mean(myacc_sklearn),np.std(myacc_sklearn,ddof=1))
+print("precison",np.mean(mypre_sklearn),np.std(mypre_sklearn,ddof=1))
+print("recall",np.mean(myrecall_sklearn),np.std(myrecall_sklearn,ddof=1))
+print("f1",np.mean(myf1_sklearn),np.std(myf1_sklearn,ddof=1))
+print("auc",np.mean(myauc_sklearn),np.std(myauc_sklearn,ddof=1))
+print("des",np.mean(metric_list),np.std(metric_list,ddof=1))
+
+
+
+# print(model_name)
+# print(np.mean(mydice2))
+#
+# performance = np.mean(metric_list, axis=0)[0]
+# mean_hd95 = np.mean(metric_list, axis=0)[1]
+# meanjaccard=np.mean(metric_list, axis=0)[2]
+# print("dice",performance)
+# print("miou",np.mean(miou))
+# print('iou heart',np.mean(iu1))
+#
+# print("iou_背景",np.mean(iu0))
+# # 准率
+#
+# # print('查看两个acc相等不')
+# print("acc2",np.mean(acc2))
+# print("acc--",np.mean(acc))
+# # 精确率
+# print("精确率",np.mean(myPrecision))
+#
+# # # 号回率
+# print("号回率",np.mean( myrecall))
+# # f1-score
+# print("f1-score",np.mean(myf1))
+# print("fb-score",np.mean(myfB))
+#
+# print("hd95",mean_hd95)
+#
+# print("jaccard",meanjaccard)
+# # print("mae",np.mean(mymae))
+# print("mae",np.mean(mymae_flatten))
+# print("mse",np.mean(mymse))
+# auc1=np.mean(myauc1)
+# auc2=np.mean(myauc2)
+# print("auc1",auc1)
+# print("auc2",auc2)
+# print(auc1==auc2)
+# print(performance,mean_hd95)
+  #
+
+    #
+    # ac=accuracy(pre.data.cpu().numpy().squeeze(),labels.data.numpy().squeeze())
+    # print("查看acc相等不")
+    # print(PA==ac)
+
+
+
+    #
+    # pre = pre.squeeze().cpu().data.numpy()  # 256 256
+    # pree = np.copy(pre)  # 256 256
+    #
+    # # print("pre")
+    #
+    # # print(pre.shape)
+    # print(set(pre.flatten()))
+    # for i in range(pre.shape[0]):
+    #     for j in range(pre.shape[1]):
+    #         if (pre[i][j] == 0):
+    #             pre[i][j] = 0
+    #         if (pre[i][j] == 1):
+    #             pre[i][j] = 80
+    #         if (pre[i][j] == 2):
+    #             pre[i][j] = 160
+    #         if (pre[i][j] == 3):
+    #             pre[i][j] = 255
+    #
+    # from PIL import Image
+    # pre1 = Image.fromarray((np.uint8(pre)))
+
+
+    # pre1.save('/home/zmk/deeplearning/CAMUS_重新开始/predictkeshihua/network_34_demo05_swin_res34_最后上采样4倍_修改/'+str(x)+'.png')
---
+++ main/train.py
@@ -0,0 +1,228 @@
+import datetime
+import os
+import time
+import torch
+import torch.backends.cudnn as cudnn
+
+from torch import nn
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+
+
+from test01 import cross_wdice_softmax
+
+
+os.environ['CUDA_ENABLE_DEVICES'] = '0'
+
+# Setup device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+# 记录每一轮训练的的lpss
+epoch_train_loss=[]
+# 记录每一轮训练的的miou
+epoch_train_miou=[]
+
+
+from torch.nn import CrossEntropyLoss
+loss_Cross= CrossEntropyLoss()
+
+loss_cro_wdice=cross_wdice_softmax(2)
+
+def fit_one_epoch(net,w1,w2,w3,epoch,gen):
+
+        print("len(gen)",len(gen))
+        net = net.train()
+        train_miou=0
+        iu0=0
+        iu1=0
+        train_total_loss = 0
+        train_dice=0
+        for i, (imgs,labels) in enumerate(gen):
+            imgs = imgs
+            # 2 1 256 256
+            labels = labels.squeeze(1)
+            # 2  256 256
+            # print("这是第多少个batchsize  ", i)
+            if torch.cuda.is_available():
+                # print("\n")
+                # 2 3 256 256
+                imgs=imgs.to(device=device)
+                # 2 1 256 256
+                # 2  256 256
+                labels=labels.to(device=device)
+            # 2 2 256 256
+            # print("imgs.shape ", imgs.shape)
+            outputs=net(imgs)
+
+            croloss1 = loss_Cross(outputs[0], labels.long())
+            croloss2 = loss_Cross(outputs[1], labels.long())
+            croloss3 = loss_Cross(outputs[2], labels.long())
+            #
+            lossx1 = loss_cro_wdice(outputs[0], labels)
+            lossx2 = loss_cro_wdice(outputs[1], labels)
+            lossx3 = loss_cro_wdice(outputs[2], labels)
+            lossx = w1* (croloss1 + lossx1) + w2* (croloss2 + lossx2) + w3 * (lossx3 + croloss3)
+
+            optimizer.zero_grad()
+            lossx.backward()
+            optimizer.step()
+
+
+def  val_one_epoch(net, w1,w2,w3, epoch, val_gen):
+    print("val")
+    net.eval()
+
+    total_eval_loss = 0
+
+    with torch.no_grad():
+        for i, (val_imgs, val_labels) in enumerate(val_gen):
+            val_labels = val_labels.squeeze(1)
+
+            if (torch.cuda.is_available()):
+                val_imgs = val_imgs.to(device=device)
+                val_labels = val_labels.long().to(device=device)
+            # print(val_imgs.shape)
+            # print(val_labels.shape)
+            outputs = net(val_imgs)
+            croloss1 = loss_Cross(outputs[0], val_labels.long())
+            eval_loss = loss_cro_wdice(outputs[0], val_labels)+croloss1
+            # outputs = outputs[0]
+
+            # total_eval_loss = total_eval_loss +0.8* eval_loss.item()+0.2*dece_loss.item()
+            total_eval_loss = total_eval_loss + eval_loss.item()
+    return total_eval_loss / len(val_gen)
+
+
+def createmode():
+
+    # for i in range(5):
+    #   1.unet 2015
+    global model, model_name
+    from mynets.hynet import net1
+    model = net1(2)
+    model_name = 'hynet'
+
+
+    model = model.to(device=device)
+
+    return model,model_name
+
+
+if __name__ == "__main__":
+
+        import random
+
+        import numpy as np
+        seed = 2021
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+        NUM_CLASSES = 2
+        w1,w2,w3=1,1,1
+
+        from train_val_test_config import BUSI_path, savepath
+
+            # ---------------------------#
+            #   读取数据集对应的txt
+            # ---------------------------#
+        with open(os.path.join(BUSI_path, "train.txt"), "r") as f:
+            train_lines = f.readlines()
+        with open(os.path.join(BUSI_path, "val.txt"), "r") as f:
+            val_lines = f.readlines()
+        train_img_ids = train_lines
+        val_img_ids = val_lines
+
+        # for i in  range(7,8):
+
+        model,model_name = createmode()
+        # model_name=model_name+'structsloss17*17'
+        # model_name=model_name+'wbce[11*11]_reallywdice'
+        # model_name =model_name +  'cro_wdice不深監'
+        # print(model)
+        print("model------------name-----")
+        print(model_name)
+        print('model')
+        print(model)
+
+        savepath='/home/zmk/deeplearning/Dataset_BUSI_upup/result'
+
+        path = savepath + model_name + '/'
+
+
+        # exit(0)
+
+        lr = 1e-2  # 将刚开始学习率设置大一点
+        Init_Epoch = 0
+        max_epoch = 100
+        Batch_size = 12
+
+        # params = model.parameters()
+        # optimizer = torch.optim.Adam(params, 0.0001, betas=(0.5, 0.999))
+        #
+        # lr_scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=1, gamma=0.92)
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr, momentum=0.9, weight_decay=1e-4)
+        iterator = tqdm(range(max_epoch), ncols=70)
+
+        start_epoch=0
+
+        from utils.dataset import DeeplabDataset
+
+        train_dataset   = DeeplabDataset(train_img_ids,BUSI_path, 'train')
+        gen = DataLoader(train_dataset, batch_size=Batch_size, num_workers=0, pin_memory=True,     drop_last=True)
+
+
+        val_dataset   = DeeplabDataset(val_img_ids,  BUSI_path,'val')
+        val_gen = DataLoader(val_dataset, batch_size=Batch_size, num_workers=0, pin_memory=True,     drop_last=True)
+
+
+        print("train length   ", len(train_img_ids))
+        print("val length   ", len(val_img_ids))
+        # 我能说一句，就这模样，学历，加老师职业，在我家那边大杀四方，无人能破
+
+        # epoch_size      = len(train_lines) // Batch_size
+        epoch_size      = len(train_img_ids) // Batch_size
+
+        if epoch_size == 0:
+            raise ValueError("数据集过小，无法进行训练，请扩充数据集。")
+        min_loss = 10000
+        maxmiou = -1
+        # for epoch in range(start_epoch,args.num_epochs ):
+        for epoch in iterator:
+        # for epoch in range(2):
+            start = time.time()
+            print("epoch======{}".format(epoch))
+            fit_one_epoch(model,w1,w2,w3, epoch, gen)
+            # print(w1, w2, w3)
+            val_loss=   val_one_epoch(model,w1,w2,w3, epoch,val_gen)
+
+            lr_ = lr * (1.0 - epoch / max_epoch) ** 0.9
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr_
+            end = time.time()
+
+
+            print(" one epoch  need time s ",end-start)
+            save_file = {"model": model.state_dict() }
+
+            if (epoch == 0):
+                if (os.path.exists(path)):
+                    print(path)
+                    raise "path 存在"
+
+            if  val_loss < min_loss:
+                min_loss = val_loss
+                print("save model")
+                # 保存模型语句
+                # torch.save(model.state_dict(), "model.pth")
+                torch.save(save_file, path + "/model_best.pth")
+
+            if (epoch == max_epoch - 1):
+               torch.save(save_file, path + "/model_{}.pth".format(epoch))
+
+        torch.cuda.empty_cache()
+        del  model
+
